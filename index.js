c9b073a3a5f33d017afbde006a662b745dc748db
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-02-08 13:41:32 +0200

    Tehtävä 3.9

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+app.use(morgan('tiny')) // HUOM!! 3.8 puuttuu!
+app.use(cors())
 
 let persons = [
       {
