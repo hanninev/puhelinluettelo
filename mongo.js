5ad274d60b70720df470e00e2216ee9c31466df4
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-02-10 18:37:54 +0200

    Tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://fullstack:salasana@ds127888.mlab.com:27888/puhelinluettelo-db'
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+if (process.argv.length > 2) {
+const person = new Person({
+  name: process.argv[2],
+  number: process.argv[3]
+})
+
+person
+  .save()
+  .then(response => {
+    console.log('lisätään henkilö ' + person.name + ' numero ' + person.number + ' luetteloon')
+    mongoose.connection.close()
+  })
+} else {
+  Person
+  .find({})
+  .then(result => {
+    console.log('puhelinluettelo:')
+    result.forEach(person => {
+      console.log(person.name + ' ' + person.number)
+    })
+    mongoose.connection.close()
+  })
+}
