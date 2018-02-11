1ee9da20bcefdabb38d709eaa78397367e422036
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-02-11 22:20:38 +0200

    Tehtävä 3.22 ja sisennykset kuntoon

--- index.js
+++ index.js
@@ -20,21 +20,21 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (body.name === undefined) {
-    return response.status(400).json({error: 'name missing'})
+    return response.status(400).json({ error: 'name missing' })
   } else if (body.number === undefined) {
-    return response.status(400).json({error: 'number missing'})
-  } 
+    return response.status(400).json({ error: 'number missing' })
+  }
 
   Person
-  .find({ name: body.name })
-  .then(result => {
-    return result.length
-  })
-  .then(resultLength => {
-    if(resultLength > 0) {
-      return response.status(400).json({error: 'name must be unique'})
-    }
-  })  
+    .find({ name: body.name })
+    .then(result => {
+      return result.length
+    })
+    .then(resultLength => {
+      if (resultLength > 0) {
+        return response.status(400).json({ error: 'name must be unique' })
+      }
+    })
 
   const person = new Person({
     name: body.name,
@@ -43,13 +43,13 @@ app.post('/api/persons', (request, response) => {
   })
 
   person
-   .save()
-   .then(person => {
-     response.json(formatPerson(person))
+    .save()
+    .then(person => {
+      response.json(formatPerson(person))
     })
     .catch(error => {
       console.log(error)
-      }) 
+    })
 })
 
 //TÄMÄ STAATTISEKSI 3.14
@@ -64,70 +64,70 @@ const formatPerson = (person) => {
 app.get('/info', (req, res) => {
   const date = new Date()
   const persons = Person.find({});
-  persons.count(function(err, count){
+  persons.count(function (err, count) {
     res.send('<p>puhelinluettelossa on ' + count + ' henkilön tiedot</p><p>' + date + '</p>')
   })
-  })
-  
+})
+
 app.get('/api/persons', (req, res) => {
-    Person
-     .find({})
-     .then(persons => {
-       res.json(persons.map(formatPerson))
-      })
-      .catch(error => {
-        console.log(error)
-        response.status(404).end()
-      })
-  })
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(formatPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
+})
 
-  app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
-  
-    const person = {
-      name: body.name,
-      number: body.number
-    }
-  
-    Person
-      .findByIdAndUpdate(request.params.id, person, { new: true } )
-      .then(person => {
-        response.json(formatPerson(person))
-      })
-      .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
-      })
-  })
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
 
-  app.delete('/api/persons/:id', (request, response) => {
-    Person
-     .findByIdAndRemove(request.params.id)
-     .then(result => {
-       response.status(204).end()
-     })
-     .catch(error => {
-       response.status(400).send({ error: 'malformatted id' })
-     })
-  })
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(person => {
+      response.json(formatPerson(person))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
 
 app.get('/api/persons/:id', (request, response) => {
-    Person
-     .findById(request.params.id)
-     .then(person => {
-       if (person) {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(formatPerson(person))
-       } else {
-         response.status(404).end()
-       }
-     })
-     .catch(error => {
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
       console.log(error)
       response.status(404).send({ error: 'malformatted id' })
     })
-  })
+})
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -10,25 +10,25 @@ const Person = mongoose.model('Person', {
 })
 
 if (process.argv.length > 2) {
-const person = new Person({
-  name: process.argv[2],
-  number: process.argv[3]
-})
-
-person
-  .save()
-  .then(response => {
-    console.log('lisätään henkilö ' + person.name + ' numero ' + person.number + ' luetteloon')
-    mongoose.connection.close()
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
   })
+
+  person
+    .save()
+    .then(response => {
+      console.log('lisätään henkilö ' + person.name + ' numero ' + person.number + ' luetteloon')
+      mongoose.connection.close()
+    })
 } else {
   Person
-  .find({})
-  .then(result => {
-    console.log('puhelinluettelo:')
-    result.forEach(person => {
-      console.log(person.name + ' ' + person.number)
+    .find({})
+    .then(result => {
+      console.log('puhelinluettelo:')
+      result.forEach(person => {
+        console.log(person.name + ' ' + person.number)
+      })
+      mongoose.connection.close()
     })
-    mongoose.connection.close()
-  })
 }
